7b9664662410c10e6dfb1f0986fb4d11761fd2ff
Imed Adel <aadelimed@gmail.com>
2022-01-23 18:21:44 +0100

    Add page

--- apps/web/src/routes.tsx
+++ apps/web/src/routes.tsx
@@ -1,21 +1,25 @@
-import { lazy } from "react";
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Routes as RRRoutes } from "react-router-dom";
 
 const App = lazy(() => import("./pages/_app"));
 const Confirm = lazy(() => import("./pages/confirm"));
 const Home = lazy(() => import("./pages/home"));
 const Login = lazy(() => import("./pages/login"));
+const Page = lazy(() => import("./pages/page"));
 
 export function Routes() {
 	return (
-		<BrowserRouter>
-			<RRRoutes>
-				<Route path="/" element={<App />}>
-					<Route index element={<Home />} />
-				</Route>
-				<Route path="/login" element={<Login />} />
-				<Route path="/confirm" element={<Confirm />} />
-			</RRRoutes>
-		</BrowserRouter>
+		<Suspense fallback={null}>
+			<BrowserRouter>
+				<RRRoutes>
+					<Route path="/" element={<App />}>
+						<Route index element={<Home />} />
+						<Route path=":id" element={<Page />} />
+					</Route>
+					<Route path="/login" element={<Login />} />
+					<Route path="/confirm" element={<Confirm />} />
+				</RRRoutes>
+			</BrowserRouter>
+		</Suspense>
 	);
 }
