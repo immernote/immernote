98cb7d5c5cfde9bc61fc3af839569ede4111f8a2
Imed Adel <aadelimed@gmail.com>
2022-01-24 21:12:45 +0100

    Add colors and fonts

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,7 +1,44 @@
+const colors = require("@radix-ui/colors")
+const plugin = require("tailwindcss/plugin")
+
+function getColors() {
+	let c = {}
+
+	for (let [name, values] of Object.entries(colors)) {
+		if (
+			![
+				"gray",
+				"red",
+				"blue",
+				"green",
+				"yellow",
+				"blackA",
+				"whiteA",
+				"amber",
+			].includes(name)
+		) {
+			continue
+		}
+
+		for (let k of Object.keys(values)) {
+			c[k] = `var(--${k})`
+		}
+	}
+
+	return c
+}
+
 module.exports = {
+	darkMode: "class",
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: { ...getColors() },
+		},
 	},
-	plugins: [],
+	plugins: [
+		require("@tailwindcss/forms"),
+		require("@tailwindcss/typography"),
+		require("@downwindcss/text-decoration"),
+	],
 }
