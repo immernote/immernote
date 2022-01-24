9e3d4eacfdb7f049e4e945fff6709b944e1ff27f
Imed Adel <aadelimed@gmail.com>
2022-01-24 21:12:46 +0100

    Update sidebar

--- apps/web/src/components/sidebar.tsx
+++ apps/web/src/components/sidebar.tsx
@@ -1,3 +1,35 @@
 export function Sidebar() {
-	return <div className="min-w-[calc(100%/5)] w-1/5 bg-gray1 h-screen">Sidebar</div>;
+	return (
+		<div className="min-w-[calc(100%/5)] w-1/5 bg-gray1 h-screen flex flex-col items-stretch">
+			<Workspaces />
+			<Links />
+			<Pages />
+			<CreatePage />
+		</div>
+	);
+}
+
+function Workspaces() {
+	return <div className="font-medium tracking-tight transition hover:bg-gray3 px-4 py-4 text-sm">Workspaces</div>;
+}
+
+function Links() {
+	return (
+		<>
+			<div className="font-medium tracking-tight transition hover:bg-gray3 px-4 py-1 text-sm">Search</div>
+			<div className="font-medium tracking-tight transition hover:bg-gray3 px-4 py-1 text-sm">Settings</div>
+		</>
+	);
+}
+
+function Pages() {
+	return <nav className="flex flex-col items-stretch w-full flex-grow min-h-[4rem] text-sm bg-gray2">Loading...</nav>;
+}
+
+function CreatePage() {
+	return (
+		<button className="font-medium tracking-tight transition hover:bg-gray3 px-4 py-2 text-sm text-left">
+			New Page
+		</button>
+	);
 }
