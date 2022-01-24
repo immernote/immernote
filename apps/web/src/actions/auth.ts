ec86e335111f1c4947a5afbb24dab177ed058195
Imed Adel <aadelimed@gmail.com>
2022-01-24 21:12:45 +0100

    `async` unnecessary

--- apps/web/src/actions/auth.ts
+++ apps/web/src/actions/auth.ts
@@ -1,10 +1,10 @@
 import type { User } from "../interfaces/user";
 import { aksios } from "../utils/aksios";
 
-export async function login(email: string) {
+export function login(email: string) {
 	return aksios<User>("/v0/login", "POST", { email });
 }
 
-export async function confirm(token: string, userID: string) {
+export function confirm(token: string, userID: string) {
 	return aksios<User>("/v0/confirm", "POST", { token, userID });
 }
