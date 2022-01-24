76ccb606d2a54734af3a8794c29a72c35e044e9e
Imed Adel <aadelimed@gmail.com>
2022-01-24 21:12:46 +0100

    Add page

--- apps/web/src/components/sidebar.tsx
+++ apps/web/src/components/sidebar.tsx
@@ -5,76 +5,67 @@ import useSWR from "swr";
 import { Space } from "../interfaces/space";
 
 export function Sidebar() {
-	return (
-		<div className="min-w-[calc(100%/5)] w-1/5 bg-gray1 h-screen flex flex-col items-stretch">
-			<Workspaces />
-			<Links />
-			<Pages />
-			<CreatePage />
-		</div>
-	);
+  return (
+    <div className="min-w-[calc(100%/5)] w-1/5 bg-gray1 h-screen flex flex-col items-stretch">
+      <Workspaces />
+      <Links />
+      <Pages />
+      <CreatePage />
+    </div>
+  );
 }
 
 function Workspaces() {
-	const { data: spaces } = useSWR("spaces", (key) => axios<Space[]>("/api/v0/spaces").then(({ data }) => data));
+  const { data: spaces } = useSWR("spaces", (key) =>
+    axios<Space[]>("/api/v0/spaces").then(({ data }) => data)
+  );
 
-	useEffect(() => {
-		console.log(spaces);
-	});
+  useEffect(() => {
+    console.log(spaces);
+  });
 
-	return (
-		<div className="font-medium tracking-tight transition hover:bg-gray3 px-4 py-4 text-sm">
-			<DropdownMenu.Root>
-				<DropdownMenu.Trigger />
-
-				<DropdownMenu.Content>
-					<DropdownMenu.Label />
-					<DropdownMenu.Item />
-
-					<DropdownMenu.Group>
-						<DropdownMenu.Item />
-					</DropdownMenu.Group>
-
-					<DropdownMenu.CheckboxItem>
-						<DropdownMenu.ItemIndicator />
-					</DropdownMenu.CheckboxItem>
-
-					<DropdownMenu.RadioGroup>
-						<DropdownMenu.RadioItem value="checked">
-							<DropdownMenu.ItemIndicator />
-						</DropdownMenu.RadioItem>
-					</DropdownMenu.RadioGroup>
-
-					<DropdownMenu.Root>
-						<DropdownMenu.TriggerItem />
-						<DropdownMenu.Content />
-					</DropdownMenu.Root>
-
-					<DropdownMenu.Separator />
-					<DropdownMenu.Arrow />
-				</DropdownMenu.Content>
-			</DropdownMenu.Root>
-		</div>
-	);
+  return (
+    <div className="font-medium tracking-tight transition hover:bg-gray3 px-4 py-4 text-sm">
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger className="focus:outline-none">Workspaces</DropdownMenu.Trigger>
+        <DropdownMenu.Content className="bg-gray2 backdrop-blur-3xl rounded shadow-lg w-80">
+          <DropdownMenu.Item className="transition bg-gray3 hover:bg-gray4 px-4 py-4 hover:outline-none">
+            Workspace 1
+          </DropdownMenu.Item>
+          <DropdownMenu.Item className="transition bg-gray3 hover:bg-gray4 px-4 py-4 hover:outline-none">
+            Workspace 2
+          </DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+    </div>
+  );
 }
 
 function Links() {
-	return (
-		<>
-			<div className="font-medium tracking-tight transition hover:bg-gray3 px-4 py-1 text-sm">Search</div>
-			<div className="font-medium tracking-tight transition hover:bg-gray3 px-4 py-1 text-sm">Settings</div>
-		</>
-	);
+  return (
+    <>
+      <div className="font-medium tracking-tight transition hover:bg-gray3 px-4 py-1 text-sm">
+        Search
+      </div>
+      <div className="font-medium tracking-tight transition hover:bg-gray3 px-4 py-1 text-sm">
+        Settings
+      </div>
+    </>
+  );
 }
 
 function Pages() {
-	return <nav className="flex flex-col items-stretch w-full flex-grow min-h-[4rem] text-sm bg-gray2">Loading...</nav>;
+  return (
+    <nav className="flex flex-col items-stretch w-full flex-grow min-h-[4rem] text-sm bg-gray2">
+      Loading...
+    </nav>
+  );
 }
 
 function CreatePage() {
-	return (
-		<button className="font-medium tracking-tight transition hover:bg-gray3 px-4 py-2 text-sm text-left">
-			New Page
-		</button>
-	);
+  return (
+    <button className="font-medium tracking-tight transition hover:bg-gray3 px-4 py-2 text-sm text-left">
+      New Page
+    </button>
+  );
 }
--- apps/web/src/pages/page.tsx
+++ apps/web/src/pages/page.tsx
@@ -1,16 +1,13 @@
 import { useParams } from "react-router-dom";
 import { Layout } from "../components/layout";
-import { Sidebar } from "../components/sidebar";
+import { Link } from "../components/link";
 
 export default function Page() {
 	const { id } = useParams<{ id: string }>();
 
 	return (
 		<Layout title="Page">
-			<div className="h-screen w-full tracking-tight flex items-center">
-				<Sidebar />
-				<main className="flex-grow h-screen w-full bg-slate-500">Main stuff</main>
-			</div>
+			<Link to={`/${id}d`}>Next one?</Link>
 		</Layout>
 	);
 }
