34760612876d12f247c8fdedc8cbd1710ced8fb0
Imed Adel <aadelimed@gmail.com>
2022-02-27 22:14:48 +0100

    Load databases in sidebar

--- packages/web/src/components/sidebar.tsx
+++ packages/web/src/components/sidebar.tsx
@@ -110,7 +110,13 @@ function Links() {
 function Pages(props: { parent_page_id?: string; level: number }) {
   useFetchPageBlocks(props.parent_page_id);
   const pages = useData(
-    useCallback((state) => state.pages[props.parent_page_id ?? "root"], [props.parent_page_id]),
+    useCallback(
+      (state) =>
+        state.pages[props.parent_page_id ?? "root"]?.map(
+          (id) => [id, state.blocks[id]?.type] as const
+        ),
+      [props.parent_page_id]
+    ),
     dequal
   );
 
@@ -132,8 +138,12 @@ function Pages(props: { parent_page_id?: string; level: number }) {
 
     return (
       <>
-        {pages.map((page) =>
-          page === props.parent_page_id ? null : <Page key={page} id={page} level={props.level} />
+        {pages.map(([page_id, page_type]) =>
+          page_id === props.parent_page_id ? null : page_type === "database" ? (
+            <Database key={page_id} id={page_id} level={props.level} />
+          ) : (
+            <Page key={page_id} id={page_id} level={props.level} />
+          )
         )}
       </>
     );
@@ -149,9 +159,13 @@ function Pages(props: { parent_page_id?: string; level: number }) {
 
   return (
     <nav className="flex flex-col items-stretch w-full flex-grow min-h-[4rem] text-sm bg-gray2 overflow-y-auto">
-      {pages.map((page) => (
-        <Page key={page} id={page} level={props.level} />
-      ))}
+      {pages.map(([page_id, page_type]) =>
+        page_type === "database" ? (
+          <Database key={page_id} id={page_id} level={props.level} />
+        ) : (
+          <Page key={page_id} id={page_id} level={props.level} />
+        )
+      )}
     </nav>
   );
 }
@@ -289,6 +303,114 @@ function CreateSubPage({ page_id }: { page_id: string }) {
       <Plus className="h-[1em]" />
     </button>
   );
+}
+
+/* ------------------------------------------ Database ------------------------------------------ */
+
+function Database({ level, id }: { id: string; level: number }) {
+  const database = useData(
+    useCallback(
+      (state) => {
+        const item = state.blocks[id] as Extract<Block, { type: "database" }> | undefined;
+        if (!item) return;
+
+        return {
+          id: item.id,
+          content: item.content,
+          format: item.format,
+        };
+      },
+      [id]
+    ),
+    dequal
+  );
+  const { data: space } = useCurrentSpace();
+  const [expanded, set_expanded] = useState(false);
+  const [has_focus, set_has_focus] = useState(false);
+  const match = useMatch(":space/:id");
+  const default_expanded = useData(
+    useCallback(
+      (state) => {
+        const params_id = match?.params.id;
+        if (!params_id) return false;
+        if (id === params_id) return true;
+
+        const block = state.blocks[params_id];
+        if (!block) return false;
+        // if (block.parent_pages_ids.indexOf(id) !== -1) return true;
+
+        return false;
+      },
+      [match?.params.id]
+    )
+  );
+
+  useEffect(() => {
+    if (default_expanded) {
+      set_expanded(true);
+    }
+  }, [default_expanded]);
+
+  return database ? (
+    <>
+      <div
+        className={clsx({
+          "flex items-center justify-between px-4 transition hover:bg-gray4 group": true,
+          "bg-gray4": has_focus,
+        })}
+        style={{
+          paddingLeft: `${level / 2 + 1}rem`,
+        }}
+      >
+        <button className="py-2 hover:bg-gray6 rounded" onClick={() => set_has_focus((v) => !v)}>
+          {expanded ? <ChevronDown className="h-[1em]" /> : <ChevronRight className="h-[1em]" />}
+        </button>
+        <Link
+          className="inline-flex items-center gap-x-2 flex-grow py-2"
+          to={`/${space?.handle}/${id}`}
+        >
+          <span>{database.format.icon.value}</span>
+          <span>{database.content.title ?? "Untitled"}</span>
+        </Link>
+        <div
+          className={clsx({
+            "inline-flex items-center gap-x-2 group-hover:visible py-2": true,
+            visible: has_focus,
+            invisible: !has_focus,
+          })}
+        >
+          <DropdownMenu.Root onOpenChange={(v) => set_has_focus(v)}>
+            <DropdownMenu.Trigger className="transition-all">
+              <MoreHorizontal className="h-[1em]" />
+            </DropdownMenu.Trigger>
+            <DropdownMenu.Content className="bg-gray1 border border-gray6 backdrop-blur-3xl rounded shadow-lg w-80">
+              <DropdownMenu.Item className="text-xs text-gray11 tracking-tight bg-gray3 transition hover:bg-gray4 px-4 py-2 hover:outline-none">
+                Delete
+              </DropdownMenu.Item>
+              <DropdownMenu.Item className="text-xs text-gray11 tracking-tight bg-gray3 transition hover:bg-gray4 px-4 py-2 hover:outline-none">
+                Add to Favorites
+              </DropdownMenu.Item>
+              <DropdownMenu.Item className="text-xs text-gray11 tracking-tight bg-gray3 transition hover:bg-gray4 px-4 py-2 hover:outline-none">
+                Duplicate
+              </DropdownMenu.Item>
+              <DropdownMenu.Item className="text-xs text-gray11 tracking-tight bg-gray3 transition hover:bg-gray4 px-4 py-2 hover:outline-none">
+                Copy Link
+              </DropdownMenu.Item>
+              <DropdownMenu.Item className="text-xs text-gray11 tracking-tight bg-gray3 transition hover:bg-gray4 px-4 py-2 hover:outline-none">
+                Rename
+              </DropdownMenu.Item>
+              <DropdownMenu.DropdownMenuSeparator className="h-px bg-gray7" />
+              <DropdownMenu.Item className="text-xs text-gray11 tracking-tight bg-gray3 transition hover:bg-gray4 px-4 py-2 hover:outline-none">
+                Move to
+              </DropdownMenu.Item>
+            </DropdownMenu.Content>
+          </DropdownMenu.Root>
+          {/* <CreateSubPage page_id={page.id} /> */}
+        </div>
+      </div>
+      {/* {expanded ? <Pages parent_page_id={id} level={level + 1} /> : null} */}
+    </>
+  ) : null;
 }
 
 /* ---------------------------------------------------------------------------------------------- */
